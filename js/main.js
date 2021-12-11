85d20f6063d1dd3d437ebbc076d4d1b4898a6707
LatekVon <latkaignacy@gmail.com>
2021-12-11 06:57:38 +0100

    please for the love of god just let me

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// mam dość - by Ignacy Łątka
+
+const MAX_W = 9, MAX_H = 33;
+
+// [stolen]
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+let a, b, c;
+
+c = a + b;
+if (c > vh * MAX_H/100) {
+  let mult = (vh * MAX_H / 100) / c;
+  a = a * mult;
+  b = b * mult;
+  c = c * mult;
+
+}
+
+document.getElementById('bar_env').style.height = a;
+document.getElementById('bar_h2o').style.height = b;
+document.getElementById('bar_oxy').style.height = c;

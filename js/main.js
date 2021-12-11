10ef7751369a4c35dde4399145ccb9bb87720b4e
LatekVon <latkaignacy@gmail.com>
2021-12-11 07:10:49 +0100

    testing with friend

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@ const MAX_W = 9, MAX_H = 33;
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-let a, b, c;
+let a = Math.random(),
+    b = Math.random();
 
-c = a + b;
+let c = a + b;
+
 if (c > vh * MAX_H/100) {
   let mult = (vh * MAX_H / 100) / c;
   a = a * mult;
   b = b * mult;
   c = c * mult;
-
 }
+
+console.log(a, ' ', b, ' ', c);
 
 document.getElementById('bar_env').style.height = a;
 document.getElementById('bar_h2o').style.height = b;
